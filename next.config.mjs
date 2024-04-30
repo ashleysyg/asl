1fe37e3ef0a0cf365fc2fa014d841a0b53333698
ashleysyg <ashley.syg.lee@gmail.com>
2024-04-30 13:00:36 -0400

    fix: images should render

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/asl",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
 
 export default nextConfig;
+
